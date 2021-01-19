6b1049d9834f2cb2d2b3003ad1e174f30e75a982
Roman Skliarenko <vurtnevk@gmail.com>
2021-01-19 19:21:20 +0200

    hw-07

---
+++ module_five.js
@@ -0,0 +1,11 @@
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+const input = document.querySelector("#name-input");
+const span = document.querySelector("#name-output");
+input.addEventListener("input", () => {
+  if (input.value !== "") {
+    span.textContent = input.value;
+  } else {
+    span.textContent = "Незнакомец";
+  }
+});
